8913102e1f078653e6dbb13e605af7371bc7f0bd
Shivam Kumar <shivambaku@gmail.com>
2024-02-07 21:25:43 -0800

    published to testchain

--- deploy.ts
+++ deploy.ts
@@ -6,29 +6,32 @@ async function main() {
     throw new Error("PRIVATE_KEY environment variable is required");
   }
 
-  console.log(privateKey);
+  const rpcURL = Bun.env.RPC_URL;
+  if (!rpcURL) {
+    throw new Error("RPC_URL environment variable is required");
+  }
 
-  // const provider = new ethers.JsonRpcProvider("HTTP://127.0.0.1:7545");
-  // const wallet = new ethers.Wallet(privateKey, provider);
+  const provider = new ethers.JsonRpcProvider(rpcURL);
+  const wallet = new ethers.Wallet(privateKey, provider);
 
-  // const abi = await Bun.file(
-  //   "./bin/SimpleStorage_sol_SimpleStorage.abi"
-  // ).text();
+  const abi = await Bun.file(
+    "./bin/SimpleStorage_sol_SimpleStorage.abi"
+  ).text();
 
-  // const binary = await Bun.file(
-  //   "./bin/SimpleStorage_sol_SimpleStorage.bin"
-  // ).text();
+  const binary = await Bun.file(
+    "./bin/SimpleStorage_sol_SimpleStorage.bin"
+  ).text();
 
-  // const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
 
-  // console.log("Deploying contract...");
-  // const contract = await contractFactory.deploy({
-  //   gasPrice: 20000000000,
-  //   gasLimit: 6721975,
-  // });
-  // console.log(contract);
+  console.log("Deploying contract...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
+  console.log("Contract deployed to:", await contract.getAddress());
+  await contract.waitForDeployment();
+  console.log("Contract deployed successfully");
 
-  // const currentFavoriteNumber = await contract.retrieve();
+  // const currentFavoriteNumber = await contract.getFunction("favoriteNumber")();
   // console.log("Current favorite number:", currentFavoriteNumber.toString());
 }
 
